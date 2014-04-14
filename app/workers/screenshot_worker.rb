b288071ee9c35e161ee8d151c20678a21b01db71
Fraser Scott <admin@privacythroughchoice.org>
2014-04-14 15:43:32 +0100

    rebrand and screenshot

--- app/workers/screenshot_worker.rb
+++ app/workers/screenshot_worker.rb
@@ -2,9 +2,9 @@ class ScreenshotWorker
   include Sidekiq::Worker
 
   def perform(url, slug)
-    image_path = "/Users/fscott/Personal/projects/webnark/webnark-rails/public/screenshots"
+    image_path = "/opt/ptc-rails/current/public/screenshots"
 
-    image = `/Users/fscott/Personal/projects/webnark/screenshot/make_screenshot.sh '#{url}'`
+    image = `/opt/screenshot/app/make_screenshot.sh '#{url}'`
 
     if not image or image.empty?
       FileUtils.cp("#{image_path}/_na.png","#{image_path}/#{slug}.png")
